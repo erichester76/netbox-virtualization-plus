008c702883818575f5140e835230d95fdfbe5149
Eric Hester <eric.hester@umbrella.tech>
2024-12-21 18:02:16 -0500

    typooooo

--- netbox_virtualization_plus/navigation.py
+++ netbox_virtualization_plus/navigation.py
@@ -2,144 +2,144 @@ from netbox.plugins import PluginMenuButton, PluginMenuItem, PluginMenu
 
 items = (
     PluginMenuItem(
-        link='plugins:virtualization_plus:datastore_list',
+        link='plugins:netbox_virtualization_plus:datastore_list',
         link_text='Datastores',
         buttons=[
             PluginMenuButton(
-                link='plugins:virtualization_plus:datastore_add',
+                link='plugins:netbox_virtualization_plus:datastore_add',
                 title='Add Datastore',
                 icon_class='mdi mdi-plus-thick',
             ),
             PluginMenuButton(
-                link='plugins:virtualization_plus:datastore_import',
+                link='plugins:netbox_virtualization_plus:datastore_import',
                 title='Import Datastores',
                 icon_class='mdi mdi-upload',
             )
         ]
     ),
     PluginMenuItem(
-        link='plugins:virtualization_plus:virtualdiskextension_list',
+        link='plugins:netbox_virtualization_plus:virtualdiskextension_list',
         link_text='Virtual Disk Extensions',
         buttons=[
             PluginMenuButton(
-                link='plugins:virtualization_plus:virtualdiskextension_add',
+                link='plugins:netbox_virtualization_plus:virtualdiskextension_add',
                 title='Add Disk Extension',
                 icon_class='mdi mdi-plus-thick',
             ),
             PluginMenuButton(
-                link='plugins:virtualization_plus:virtualdiskextension_import',
+                link='plugins:netbox_virtualization_plus:virtualdiskextension_import',
                 title='Import Disk Extensions',
                 icon_class='mdi mdi-upload',
             )
         ]
     ),
     PluginMenuItem(
-        link='plugins:virtualization_plus:virtualswitch_list',
+        link='plugins:netbox_virtualization_plus:virtualswitch_list',
         link_text='Virtual Switches',
         buttons=[
             PluginMenuButton(
-                link='plugins:virtualization_plus:virtualswitch_add',
+                link='plugins:netbox_virtualization_plus:virtualswitch_add',
                 title='Add Virtual Switch',
                 icon_class='mdi mdi-plus-thick',
             ),
             PluginMenuButton(
-                link='plugins:virtualization_plus:virtualswitch_import',
+                link='plugins:netbox_virtualization_plus:virtualswitch_import',
                 title='Import Virtual Switches',
                 icon_class='mdi mdi-upload',
             )
         ]
     ),
     PluginMenuItem(
-        link='plugins:virtualization_plus:virtualnetwork_list',
+        link='plugins:netbox_virtualization_plus:virtualnetwork_list',
         link_text='Virtual Networks',
         buttons=[
             PluginMenuButton(
-                link='plugins:virtualization_plus:virtualnetwork_add',
+                link='plugins:netbox_virtualization_plus:virtualnetwork_add',
                 title='Add Virtual Network',
                 icon_class='mdi mdi-plus-thick',
             ),
             PluginMenuButton(
-                link='plugins:virtualization_plus:virtualnetwork_import',
+                link='plugins:netbox_virtualization_plus:virtualnetwork_import',
                 title='Import Virtual Networks',
                 icon_class='mdi mdi-upload',
             )
         ]
     ),
     PluginMenuItem(
-        link='plugins:virtualization_plus:virtualswitchuplink_list',
+        link='plugins:netbox_virtualization_plus:virtualswitchuplink_list',
         link_text='Switch Uplinks',
         buttons=[
             PluginMenuButton(
-                link='plugins:virtualization_plus:virtualswitchuplink_add',
+                link='plugins:netbox_virtualization_plus:virtualswitchuplink_add',
                 title='Add Switch Uplink',
                 icon_class='mdi mdi-plus-thick',
             ),
             PluginMenuButton(
-                link='plugins:virtualization_plus:virtualswitchuplink_import',
+                link='plugins:netbox_virtualization_plus:virtualswitchuplink_import',
                 title='Import Switch Uplinks',
                 icon_class='mdi mdi-upload',
             )
         ]
     ),
     PluginMenuItem(
-        link='plugins:virtualization_plus:vminterfaceextension_list',
+        link='plugins:netbox_virtualization_plus:vminterfaceextension_list',
         link_text='VM Interface Extensions',
         buttons=[
             PluginMenuButton(
-                link='plugins:virtualization_plus:vminterfaceextension_add',
+                link='plugins:netbox_virtualization_plus:vminterfaceextension_add',
                 title='Add Interface Extension',
                 icon_class='mdi mdi-plus-thick',
             ),
             PluginMenuButton(
-                link='plugins:virtualization_plus:vminterfaceextension_import',
+                link='plugins:netbox_virtualization_plus:vminterfaceextension_import',
                 title='Import Interface Extensions',
                 icon_class='mdi mdi-upload',
             )
         ]
     ),
     PluginMenuItem(
-        link='plugins:virtualization_plus:resourcepool_list',
+        link='plugins:netbox_virtualization_plus:resourcepool_list',
         link_text='Resource Pools',
         buttons=[
             PluginMenuButton(
-                link='plugins:virtualization_plus:resourcepool_add',
+                link='plugins:netbox_virtualization_plus:resourcepool_add',
                 title='Add Resource Pool',
                 icon_class='mdi mdi-plus-thick',
             ),
             PluginMenuButton(
-                link='plugins:virtualization_plus:resourcepool_import',
+                link='plugins:netbox_virtualization_plus:resourcepool_import',
                 title='Import Resource Pools',
                 icon_class='mdi mdi-upload',
             )
         ]
     ),
     PluginMenuItem(
-        link='plugins:virtualization_plus:resourcescheduling_list',
+        link='plugins:netbox_virtualization_plus:resourcescheduling_list',
         link_text='Resource Scheduling',
         buttons=[
             PluginMenuButton(
-                link='plugins:virtualization_plus:resourcescheduling_add',
+                link='plugins:netbox_virtualization_plus:resourcescheduling_add',
                 title='Add Resource Scheduling',
                 icon_class='mdi mdi-plus-thick',
             ),
             PluginMenuButton(
-                link='plugins:virtualization_plus:resourcescheduling_import',
+                link='plugins:netbox_virtualization_plus:resourcescheduling_import',
                 title='Import Schedulings',
                 icon_class='mdi mdi-upload',
             )
         ]
     ),
     PluginMenuItem(
-        link='plugins:virtualization_plus:highavailability_list',
+        link='plugins:netbox_virtualization_plus:highavailability_list',
         link_text='HA Configs',
         buttons=[
             PluginMenuButton(
-                link='plugins:virtualization_plus:highavailability_add',
+                link='plugins:netbox_virtualization_plus:highavailability_add',
                 title='Add HA Config',
                 icon_class='mdi mdi-plus-thick',
             ),
             PluginMenuButton(
-                link='plugins:virtualization_plus:highavailability_import',
+                link='plugins:netbox_virtualization_plus:highavailability_import',
                 title='Import HA Configs',
                 icon_class='mdi mdi-upload',
             )
